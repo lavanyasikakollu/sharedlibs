4216fdfb748ce367fdf196cabf3ba23f4a8261af
lavanyasikakollu <lavanyas534@gmail.com>
2022-01-05 15:44:17 +0530

    added functions

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,3 +1,12 @@
 def call(name){
    echo "hello ${name}- How are you ?"
 }
+def add(x,y) {
+  echo "addition of ${x} and ${y} is ${x+y}"
+}
+def sub(x,y) {
+  echo "addition of ${x} and ${y} is ${x-y}"
+}
+def mul(x,y) {
+  echo "addition of ${x} and ${y} is ${x*y}"
+}
